3cf74c4b88fd4bec2e6c5557f4b72505758dc9bd
Itamar Sahar <itamarsar81@gmail.com>
2022-06-07 14:35:34 +0300

    deploing

--- api/auth/auth.routes.js
+++ api/auth/auth.routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const {login, signup, logout} = require('./auth.controller')
+const { log } = require('../../middlewares/logger.middleware')
 
 const router = express.Router()
 
-router.post('/login', login)
-router.post('/signup', signup)
+router.post('/login',log, login)
+router.post('/signup',log, signup)
 router.post('/logout', logout)
 
 module.exports = router
